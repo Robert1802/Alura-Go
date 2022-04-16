f9158071ef532c1fc7e3ec9790bff04db39ce9ab
Roberto Moura <roblm_@hotmail.com>
2022-04-16 18:37:53 -0300

    Controle de erros e lendo arquivos

--- hello.go
+++ hello.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 2
 
 func main() {
+
+	exibeIntroducao()
+
+	for {
+
+		exibeMenu()
+
+		comando := leComando()
+
+		// if comando == 1 {
+		// 	fmt.Println("Monitorando...")
+		// } else if comando == 2 {
+		// 	fmt.Println("Exibindo logs...")
+		// } else if comando == 0 {
+		// 	fmt.Println("Saindo...")
+		// } else {
+		// 	fmt.Println("Não conheço este comando")
+		// }
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func exibeIntroducao() {
+
 	// var nome string = "Roberto"
 	// var versao float32 = 1.18
 	// var idade int = 29
@@ -13,18 +61,107 @@ func main() {
 	fmt.Println("Hellooooo, ", nome)
 	fmt.Println("Este programa esta na versao, ", versao)
 
+}
+
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
+}
 
-	var comando int
+func leComando() int {
+	var comandoLido int
 	// "%d" representa um inteiro
-	// o Scanf recebe um modificado "%d" e atribui à variavel comando
-	// o & antes do &comando representa o endereço da variavel, um ponteiro para a variavel
-	//fmt.Scanf("%d", &comando)
+	// o Scanf recebe um modificado "%d" e atribui à variavel comandoLido
+	// o & antes do &comandoLido representa o endereço da variavel, um ponteiro para a variavel
+	//fmt.Scanf("%d", &comandoLido)
 	// Scan recebe um int
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da variavel comando é: ", &comando)
-	fmt.Println("O comando escolhido foi ", comando)
+	fmt.Scan(&comandoLido)
+	//fmt.Println("O endereço da variavel comandoLido é: ", &comandoLido)
+	fmt.Println("O comandoLido escolhido foi ", comandoLido)
+	return comandoLido
+}
 
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	fmt.Println("")
+
+	// sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+	sites := leSitesDoArquivo()
+
+	// for i := 0; i < len(sites); i++ {
+	// ou
+	// for i, site := range sites {
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando o site [", site, "] que está na posição ", i, " do meu Slice")
+			testaSite(site)
+		}
+		// Aguarda 2 segundos antes do proximo monitoramento
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("O erro foi: ", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site: ", site, " foi carregado corretamente")
+	} else {
+		fmt.Println("Site: ", site, " foi está com problemas. Status Code: ", resp.StatusCode)
+	}
+}
+
+func exemploDeSlice() {
+	//Isso é um Slice, uma abstração da array do Go com tamanho variavel
+	nomes := []string{"Nome 1", "Nome 2", "Nome 3"}
+	fmt.Println(nomes)
+	fmt.Println("O meu Slice tem:", len(nomes), " itens")
+	fmt.Println("A capacidade do Slice é:", cap(nomes), " itens")
+
+	// O slice dobra a sua capacidade antes de estourar com append
+	nomes = append(nomes, "Nome 4")
+	fmt.Println(nomes)
+	fmt.Println("O meu slice tem:", len(nomes), " itens")
+	fmt.Println("A capacidade do Slice é:", cap(nomes), " itens")
+
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	//arquivo, err := ioutil.ReadFile("sites.txt")
+	if err != nil {
+		fmt.Println("O erro foi: ", err)
+	}
+
+	// bufio vai ler o arquivo linha a linha
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		// a quebra de linha ocorre no \n que é a quebra de linha
+		linha, err := leitor.ReadString('\n')
+		// retira o Enter extra ou os espaços sobrando
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	arquivo.Close()
+
+	return sites
 }
